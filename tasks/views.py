e551eaca70e4c82f6cb48f163c808b79872504f1
Se2my00of5 <103197402+Se2my00of5@users.noreply.github.com>
2024-09-25 20:10:46 +0700

    finish

---
+++ tasks/views.py
@@ -0,0 +1,56 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Task
+from drf_yasg import openapi
+from .serializers import TaskSerializer
+from drf_yasg.utils import swagger_auto_schema
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'description': openapi.Schema(type=openapi.TYPE_STRING, description='Описание задачи'),
+                    'completed': openapi.Schema(type=openapi.TYPE_BOOLEAN, description='Статус задачи'),
+                },
+                required=['description', 'completed'],
+            ),
+        ),
+        responses={201: TaskSerializer(many=True)}
+    )
+    # Метод для загрузки нового списка дел
+    @action(detail=False, methods=['post'])
+    def load_tasks(self, request):
+        serializer = TaskSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            Task.objects.all().delete()
+            serializer.save()  # Сохраняем все задачи
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'description': openapi.Schema(type=openapi.TYPE_STRING, description='Описание задачи'),
+                    'completed': openapi.Schema(type=openapi.TYPE_BOOLEAN, description='Статус задачи'),
+                },
+                required=['description', 'completed'],
+            ),
+        ),
+        responses={201: TaskSerializer(many=True)}
+    )
+    def create(self, request, *args, **kwargs):
+        serializer = TaskSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()  # Сохраняем все задачи
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
